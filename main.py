a57e828490db893a42f5a8fd1618368ac2203ffb
Tolentino-MaRose <maroseltolentino@iskolarngbayan.pup.edu.ph>
2025-04-08 17:19:41 +0800

    add add_record function

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
+import os
+
+def clear():
+    os.system("cls")
+
 def list_all_records(record_list):
+    
     if not record_list:
         print("No records found.")
     else:
-        print("\nList of All Member(s):")
+        print("\nList of All Users:")
         counter = 1
         for record in record_list:
             print(f"\nMember {counter}:")
             for key, value in record.items():
                 print(f"{key}: {value}")
             counter += 1
+    
+    input("\nPress Enter to return to the main menu...")
 
-# TODO (TOLENTINO): Create a function to add a record 
-def add_record():
-    pass
+def add_record(record_list):
+    
+    user_name = input("\nEnter your name (LN, FN MI) : ")
+    course_section = input("\nEnter your course and section : ")
+    programming_language = input("\nEnter your preferred programming "
+                                "language (ex: Python) : ")
+    role = input("\nEnter your preferred role (ex: Backend) : ")
+    skill_level = input("\nEnter your skill level (Beginner, Intermediate, "
+                        "Advanced) : ")
+
+    user_record = {
+        "user_name"             : user_name,
+        "course_section"        : course_section,
+        "programming_language"  : programming_language,
+        "role"                  : role,
+        "skill_level"           : skill_level
+    }
+
+    record_list.append(user_record)
+    
+    input("\nUser Record added successfully. Press Enter to return "
+          "to the main menu...")
+        
 
 # TODO (BAYOS): Create a function to update a record
 def update_record():
@@ -26,26 +54,20 @@ def delete_record():
 def search_record():
     pass
 
+
 def main():
-    record_list = [
-        {
-        "Name": "Jaira Isabel F. Ocariza",
-        "Course & Section": "BSIT 2-1",
-        "Preferred Programming Language": "Python",
-        "Preferred Role": "Backend Developer",
-        "Skill Level": "Beginner"
-        }
-    ]
+    record_list = []
     while True:
+        clear()
         print("\nHIGHPYVE TECH PROFILE")
-        print("1 - List All")
+        print("\n1 - List All")
         print("2 - Add")
         print("3 - Update")
         print("4 - Delete")
         print("5 - Search")
         print("6 - Exit")
 
-        choice = input("Enter your choice: ")
+        choice = input("\nEnter your choice: ")
 
         if not choice.isnumeric():
             print("Please enter a valid number.")
@@ -55,14 +77,19 @@ def main():
 
         match choice:
             case 1:
+                clear()
                 list_all_records(record_list)
             case 2:
-                add_record()
+                clear()
+                add_record(record_list)
             case 3:
+                clear()
                 update_record()
             case 4:
+                clear()
                 delete_record()
             case 5:
+                clear()
                 search_record()
             case 6:
                 print("Exiting HighPYve Tech Profile. Goodbye!")
