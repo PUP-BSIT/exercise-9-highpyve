9f4a532418a934f31a029a34a34769486867d707
Ocariza_JairaIsabel <jairaocariza@gmail.com>
2025-04-08 15:16:21 +0800

    Add main.py with TODO list

---
+++ main.py
@@ -0,0 +1,24 @@
+# TODO (OCARIZA): Create a function to show all records in the list
+def list_all_records():
+    pass
+
+# TODO (TOLENTINO): Create a function to add a record 
+def add_record():
+    pass
+
+# TODO (BAYOS): Create a function to update a record
+def update_record():
+    pass
+
+# TODO (BARTOLOME): Create a function to delete a record 
+def delete_record():
+    pass
+
+# TODO (ANIPAN): Create a function to search for a specific record
+def search_record():
+    pass
+
+# TODO (ALL): Create the main function to run the menu and handle choices
+def main():
+    pass
+main()
